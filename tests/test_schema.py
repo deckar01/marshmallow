deckar01/marshmallow

commit 5a933dc882e21af5e94a50bb99f62261cc8566a0

Add type annotations and mypy checking (#1259)
* start using some type annotations

* typing, mypy, and dropping py3.5 support

* add set as a type

* make nested's type Schema instead of SchemaABC

* Run mypy with pre-commit

* Address mypy errors

* Restore Python 3.5 compatibility

* More py35 reversion

* Add num_type to _BaseNumber

* Type num_type

* Privatize _BaseNumber

* Distribute type information

per https://www.python.org/dev/peps/pep-0561/

* Type more of fields module

* More typing in utils

* Update changelog

src/marshmallow/class_registry.py
@@ -7,16 +7,20 @@ class:`fields.Nested <marshmallow.fields.Nested>`.
     This module is treated as private API.
     Users should not need to use this module directly.
 """
+import typing
 from marshmallow.exceptions import RegistryError
+
+if typing.TYPE_CHECKING:
+    from marshmallow import Schema
 
 # {
 #   <class_name>: <list of class objects>
 #   <module_path_to_class>: <list of class objects>
 # }
-_registry = {}
+_registry = {}  # type: typing.Dict[str, typing.List["Schema"]]
 
 
-def register(classname, cls):
+def register(classname: str, cls: "Schema") -> None:
     """Add a class to the registry of serializer classes. When a class is
     registered, an entry for both its classname and its full, module-qualified
     path are added to the registry.
@@ -58,7 +62,9 @@ def register(classname, cls):
     return None
 
 
-def get_class(classname, all=False):
+def get_class(
+    classname: str, all: bool = False
+) -> typing.Union[typing.List["Schema"], "Schema"]:
     """Retrieve a class from the registry.
 
     :raises: marshmallow.exceptions.RegistryError if the class cannot be found

.gitignore
@@ -74,3 +74,8 @@ venv
 # Other
 .directory
 *.pprof
+
+# mypy
+.mypy_cache/
+.dmypy.json
+dmypy.json

.pre-commit-config.yaml
@@ -14,6 +14,10 @@ repos:
   hooks:
   - id: flake8
     additional_dependencies: ['flake8-bugbear==19.8.0']
+- repo: https://github.com/pre-commit/mirrors-mypy
+  rev: v0.720
+  hooks:
+  - id: mypy
 - repo: https://github.com/asottile/blacken-docs
   rev: v1.3.0
   hooks:

examples/flask_example.py
@@ -12,13 +12,13 @@ db = SQLAlchemy(app)
 ##### MODELS #####
 
 
-class Author(db.Model):
+class Author(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     first = db.Column(db.String(80))
     last = db.Column(db.String(80))
 
 
-class Quote(db.Model):
+class Quote(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))

performance/benchmark.py
@@ -24,9 +24,9 @@ class AuthorSchema(Schema):
     book_count = fields.Float()
     age = fields.Float()
     address = fields.Str()
-    full_name = fields.Method("full_name")
+    full_name = fields.Method("get_full_name")
 
-    def full_name(self, author):
+    def get_full_name(self, author):
         return "{}, {}".format(author.last, author.first)
 
 

setup.cfg
@@ -13,3 +13,6 @@ select = B,C,E,F,W,T4,B9
 [tool:pytest]
 norecursedirs = .git .ropeproject .tox docs env venv
 addopts = -v --tb=short
+
+[mypy]
+ignore_missing_imports = true

setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 
 EXTRAS_REQUIRE = {
     "tests": ["pytest", "pytz", "simplejson"],
-    "lint": ["flake8==3.7.8", "flake8-bugbear==19.8.0", "pre-commit~=1.17"],
+    "lint": [
+        "mypy==0.720",
+        "flake8==3.7.8",
+        "flake8-bugbear==19.8.0",
+        "pre-commit~=1.17",
+    ],
     "docs": [
         "sphinx==2.2.0",
         "sphinx-issues==1.2.0",
@@ -50,6 +55,7 @@ setup(
     url="https://github.com/marshmallow-code/marshmallow",
     packages=find_packages("src", exclude=("test*", "examples")),
     package_dir={"": "src"},
+    package_data={"marshmallow": ["py.typed"]},
     include_package_data=True,
     extras_require=EXTRAS_REQUIRE,
     license="MIT",

src/marshmallow/decorators.py
@@ -69,7 +69,7 @@ VALIDATES = "validates"
 VALIDATES_SCHEMA = "validates_schema"
 
 
-def validates(field_name):
+def validates(field_name: str):
     """Register a field validator.
 
     :param str field_name: Name of the field that the method validates.

commit 6ada863d553fc4a6375312c7139338e375360054

Fix typing in class_registry
It processes Schema Type, not Schema instance
https://docs.python.org/3/library/typing.html#typing.Type

Caused by #1259

src/marshmallow/class_registry.py
@@ -11,16 +11,19 @@ import typing
 from marshmallow.exceptions import RegistryError
 
 if typing.TYPE_CHECKING:
-    from marshmallow import Schema
+    # noinspection PyUnresolvedReferences
+    from marshmallow import Schema  # noqa: F401
+
+SchemaType = typing.Type["Schema"]
 
 # {
 #   <class_name>: <list of class objects>
 #   <module_path_to_class>: <list of class objects>
 # }
-_registry = {}  # type: typing.Dict[str, typing.List["Schema"]]
+_registry = {}  # type: typing.Dict[str, typing.List[SchemaType]]
 
 
-def register(classname: str, cls: "Schema") -> None:
+def register(classname: str, cls: SchemaType) -> None:
     """Add a class to the registry of serializer classes. When a class is
     registered, an entry for both its classname and its full, module-qualified
     path are added to the registry.
@@ -64,7 +67,7 @@ def register(classname: str, cls: "Schema") -> None:
 
 def get_class(
     classname: str, all: bool = False
-) -> typing.Union[typing.List["Schema"], "Schema"]:
+) -> typing.Union[typing.List[SchemaType], SchemaType]:
     """Retrieve a class from the registry.
 
     :raises: marshmallow.exceptions.RegistryError if the class cannot be found

tests/test_schema.py
@@ -2880,3 +2880,13 @@ class TestFromDict:
         dumped = OSchema().dump({"foo": 42, "bar": 24})
         assert isinstance(dumped, OrderedDict)
         assert "bar" not in dumped
+
+
+def test_class_registry_returns_schema_type():
+    class DefinitelyUniqueSchema(Schema):
+        """
+        Just a schema
+        """
+
+    SchemaClass = class_registry.get_class(DefinitelyUniqueSchema.__name__)
+    assert SchemaClass is DefinitelyUniqueSchema